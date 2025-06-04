bb1dadfc73499f241c1d37f24b14bc3eea7c9658
yyyrk <andrei.iurik@gmail.com>
2025-06-04 12:59:06 +0300

    fix(refactoring): separate logic

--- config.ru
+++ config.ru
@@ -1,3 +1,10 @@
+require_relative 'time_formatter'
 require_relative 'time_app'
 
-run TimeApp.new
+use Rack::ContentType, 'text/plain'
+
+ROUTES = {
+  '/time' => TimeApp.new
+}
+
+run Rack::URLMap.new(ROUTES)
--- time_app.rb
+++ time_app.rb
@@ -1,41 +1,27 @@
-class TimeApp
-  VALID_FORMATS = %w[year month day hour minute second].freeze
+require_relative 'time_formatter'
 
+class TimeApp
   def call(env)
     request = Rack::Request.new(env)
+    
+    return method_not_allowed unless request.get?
 
-    return not_found unless request.path == '/time' && request.get?
-
-    format_param = request.params['format']
-    return bad_request('Unknown time format []') if format_param.nil?
-
-    formats = format_param.split(',')
-    unknown_formats = formats - VALID_FORMATS
-
-    return bad_request("Unknown time format [#{unknown_formats.join(', ')}]") if unknown_formats.any?
-
-    time = Time.now
-    result = formats.map do |format|
-      case format
-      when 'year'   then time.strftime('%Y')
-      when 'month'  then time.strftime('%m')
-      when 'day'    then time.strftime('%d')
-      when 'hour'   then time.strftime('%H')
-      when 'minute' then time.strftime('%M')
-      when 'second' then time.strftime('%S')
-      end
-    end.join('-')
-
-    [200, { 'Content-Type' => 'text/plain' }, [result]]
+    handle_request(request)
   end
 
   private
 
-  def not_found
-    [404, { 'Content-Type' => 'text/plain' }, []]
+  def handle_request(request)
+    result = TimeFormatter.call(request.params['format'])
+
+    if result[:success]
+      [200, {}, [result[:result]]]
+    else
+      [400, {}, [result[:error]]]
+    end
   end
 
-  def bad_request(message)
-    [400, { 'Content-Type' => 'text/plain' }, [message]]
+  def method_not_allowed
+    [405, {}, ['Method Not Allowed']]
   end
 end
